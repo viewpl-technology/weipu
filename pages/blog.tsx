27751ccd862dba9a77f090691db72274cccffff3
Rob L <viewpl@gmail.com>
2023-02-11 01:26:43 +1100

    Update home page, add pricing and blog

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,9 @@
 export default function Blog() {
   return (
-    <>
-      <section>
+    <section className='bg-white dark:bg-gray-900'>
+      <div className='max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6'>
         <h1>This is Blog</h1>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
