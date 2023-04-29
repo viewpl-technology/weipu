834f7c841663541555afab2e5824bc1778695ff7
viewpl <viewpl@gmail.com>
2023-04-30 02:59:26 +1000

    chore: update landwind info

--- components/HeadPart.tsx
+++ components/HeadPart.tsx
@@ -1,52 +1,42 @@
-import Script from 'next/script';
-import Head from 'next/head';
+import Script from 'next/script'
+import Head from 'next/head'
 
 export default function HeadPart() {
   return (
     <>
       <Head>
-        <link
-          rel='canonical'
-          href='https://https://demo.themesberg.com/landwind/'
-        />
         <meta charSet='UTF-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <title>Landwind - Tailwind CSS Landing Page Demo</title>
+        <title>Viewpl Technology</title>
 
         {/* <!-- Meta SEO --> */}
-        <meta name='title' content='Landwind - Tailwind CSS Landing Page' />
-        <meta
-          name='description'
-          content='Get started with a free and open-source landing page built with Tailwind CSS and the Flowbite component library.'
-        />
+        <meta name='title' content='Viewpl Technology' />
+        <meta name='description' content='Internet, web & online services' />
         <meta name='robots' content='index, follow' />
         <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
         <meta name='language' content='English' />
-        <meta name='author' content='Themesberg' />
+        <meta name='author' content='Viewpl Technology' />
 
         {/* <!-- Social media share --> */}
-        <meta
-          property='og:title'
-          content='Landwind - Tailwind CSS Landing Page'
-        />
+        <meta property='og:title' content='Viewpl Technology' />
         <meta property='og:site_name' content='Themesberg' />
         <meta
           property='og:url'
-          content='https://https://demo.themesberg.com/landwind/'
+          content='https://viewpl.com,https://viewpl.com.au'
         />
         <meta
           property='og:description'
-          content='Get started with a free and open-source landing page for Tailwind CSS built with the Flowbite component library featuring dark mode, hero sections, pricing cards, and more.'
+          content='Internet, web & online services'
         />
         <meta property='og:type' content='' />
-        <meta
+        {/* <meta
           property='og:image'
           content='https://themesberg.s3.us-east-2.amazonaws.com/public/github/landwind/og-image.png'
-        />
-        <meta name='twitter:card' content='summary' />
+        /> */}
+        {/* <meta name='twitter:card' content='summary' />
         <meta name='twitter:site' content='@themesberg' />
-        <meta name='twitter:creator' content='@themesberg' />
+        <meta name='twitter:creator' content='@themesberg' /> */}
 
         {/* <!-- Favicon --> */}
         <link
@@ -71,5 +61,5 @@ export default function HeadPart() {
         <meta name='theme-color' content='#ffffff' />
       </Head>
     </>
-  );
+  )
 }
