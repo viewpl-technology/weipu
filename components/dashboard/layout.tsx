4ce7a1068063586d736b53c438edfa4f8ed26018
viewpl <viewpl@gmail.com>
2023-06-07 05:22:06 +1000

    feat: add dashboard example contents

--- components/dashboard/layout.tsx
+++ components/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import HeadPart from './HeadPart'
 import Header from './Header'
+import Aside from './Aside'
 import Footer from './Footer'
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({
@@ -11,8 +12,22 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({
     <>
       <HeadPart />
       <Header />
-      {children}
-      <Footer />
+      <div className='flex overflow-hidden bg-white pt-16'>
+        <Aside />
+        <div
+          className='bg-gray-900 opacity-50 hidden fixed inset-0 z-10'
+          id='sidebarBackdrop'
+        />
+        <div
+          id='main-content'
+          className='h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64'
+        >
+          <main>
+            <div className='pt-6 px-4'>{children}</div>
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
