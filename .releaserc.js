2a6ca9d7e3f240df7df4bfb082028e9ad02399a0
Rob Lao <viewpl@gmail.com>
2024-10-29 02:37:14 +1100

    feat: add semantic-release for versioning

---
+++ .releaserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  dryRun: true,
+  ci: false,
+}
