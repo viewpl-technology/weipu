e95a7cf00387dc2eead6126d15c32e495682ab81
Rob Lao <viewpl@gmail.com>
2025-04-06 13:46:39 +1000

    fix: add commit hash in ENV

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,6 +2,7 @@ interface Window {
   ENV?: {
     NEXT_PUBLIC_SUPABASE_URL?: string
     NEXT_PUBLIC_SUPABASE_ANON_KEY?: string
+    NEXT_PUBLIC_COMMIT_HASH?: string
     [key: string]: string | undefined
   }
 }
