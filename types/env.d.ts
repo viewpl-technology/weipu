9c6e6f4c160ef1596e858410053aaf6db92071b3
Rob Lao <viewpl@gmail.com>
2025-04-06 22:30:17 +1000

    fix: add more info in ENV

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,7 +2,9 @@ interface Window {
   ENV?: {
     NEXT_PUBLIC_SUPABASE_URL?: string
     NEXT_PUBLIC_SUPABASE_ANON_KEY?: string
-    NEXT_PUBLIC_COMMIT_HASH?: string
+    NEXT_PUBLIC_RELEASE_COMMIT_HASH?: string
+    NEXT_PUBLIC_RELEASE_VERSION?: string
+    NEXT_PUBLIC_RELEASE_DATETIME?: string
     [key: string]: string | undefined
   }
 }
