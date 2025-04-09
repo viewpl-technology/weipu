1bb13dcb36663d5c3e1c2bd545a70b561fad1a82
Rob Lao <viewpl@gmail.com>
2025-04-10 02:09:28 +1000

    fix: upgrade packages nextjs 15

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import styles from '../styles/home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
